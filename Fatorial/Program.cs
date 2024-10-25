657db42e9d82b3bf01ae4ee1deab025c10a9a961
unknown <isabela.castro@inventsoftware.com.br>
2024-10-25 13:53:30 -0300

    Feito exercicios de pesquisa binarias e fatorial ultilizando recursividade.

---
+++ Fatorial/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int Fatorial(int numero)
+{
+    if(numero == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return numero * Fatorial(numero - 1);
+    }
+}
+
+Console.WriteLine(Fatorial(2));
---
+++ PesquisaBinaria/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+var listaOrdenada = new List<int>()
+{
+    1, 2, 3, 4, 5, 6, 7, 8
+};
+
+var itemEscolhido = listaOrdenada[2];
+
+var baixo = 0;
+var alto =  listaOrdenada.Count - 1;
+
+while (baixo <= alto)
+{
+    var meio = (baixo + alto) / 2;
+
+    if(meio == itemEscolhido)
+    {
+        Console.WriteLine($"O item escolhido é {meio}");
+        break;
+    }else 
+    if(meio > itemEscolhido)
+    {
+       alto = meio - 1;
+    }
+    else
+    {
+        baixo = meio + 1;
+    }
+
+}
+
